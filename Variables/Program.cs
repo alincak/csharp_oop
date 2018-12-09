d1d4073d6010df59e08153b14af3b1f9c3092fd6
Kerem ALİNCAK <keremalincak@gmail.com>
2018-12-09 18:44:51 +0300

    variables

---
+++ Variables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Variables
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         int _x = 0;
+         int _y = 1;
+
+         var x = 0;
+         var y = 1;
+
+         Console.WriteLine("Toplamları: " + (x + y));
+         Console.ReadKey();
+      }
+   }
+}
