50c0f2770cd1dae9ce2b882e2d4fc3461d408d57
Kerem ALİNCAK <keremalincak@gmail.com>
2018-12-09 19:04:15 +0300

    methods parameter and return type

---
+++ Methods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Methods
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         MyWriteLine(Sum(10, 5));
+      }
+
+      static int Sum(int x, int y) // Sum-int-int
+      {
+         var _sum = x + y;
+         return _sum;
+      }
+
+      //static int Sum(int x, int y) // Sum-int-int // Error
+      //{
+      //   var _sum = x + y;
+      //   return _sum;
+      //}
+
+      static void MyWriteLine(int s)
+      {
+         //Sadece yapması gereken işi yapar.
+         Console.WriteLine(s);
+      }
+
+   }
+}
