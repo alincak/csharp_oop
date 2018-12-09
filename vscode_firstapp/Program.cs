cfb60dd609d8095630d56465586f27e6e0da9e80
Kerem ALİNCAK <keremalincak@gmail.com>
2018-12-09 18:22:52 +0300

    vscode firstapp

---
+++ vscode_firstapp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace vscode_firstapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = "Hello World!";
+            Console.WriteLine(x);
+        }
+    }
+}
